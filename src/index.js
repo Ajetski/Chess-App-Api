e126300947a8e75df6644c1eb194e4cfd00afbb8
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-17 17:20:47 -0500

    remove console logs

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ let store = {};
 
 wss.on('connection', ws => {
     ws.on('message', message => {
-        console.log(message);
         const action = JSON.parse(message);
         store.games = gameReducer(store.games, action, ws);
-
     });
-    console.log('client connected');
 });
 
 wss.on('close', ws => {
     // clean up 
 });
 
-console.log('WebSocket Server listening on port 8080...')
+console.log('WebSocket Server listening on port 8080...');
