a4aa8e7d1f0131cb6e048d9202b62c5e67d63f2c
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-20 21:58:03 -0500

    fix game/move functionality

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,12 +1,12 @@
 import { updatePGN } from '../utils.js';
 import { makeMove, newGame, connectToGame } from '../actions/gameActions.js';
-import { stat } from 'fs';
 
 const initialState = {};
 let gameCounter = 0;
 
 export function gameReducer(state = initialState, action, ws) {
-	if (action.type === 'game/connect') {
+	if (action.type === 'game/connect' && state[action.id]) {
+		// connecting to existing game
 		ws.send(connectToGame({
 			id: action.id,
 			orientation: state[action.id].connections.length === 1
@@ -36,15 +36,17 @@ export function gameReducer(state = initialState, action, ws) {
 			}
 		};
 	}
-	else if (action.type === 'game/move') {
-		state[action.id].connections.forEach(conn => {
-			conn.send(makeMove(updatePGN(action.move)))
-		});
+	else if (action.type === 'game/move' && state[action.id]) {
+		state[action.id].connections
+			.filter(conn => conn !== ws)
+			.forEach(conn => conn.send(makeMove({
+				pgn: updatePGN(state[action.id].pgn, action.move)
+			})));
 		return {
 			...state,
 			[action.id]: {
 				...state[action.id],
-				pgn: updatePGN(action.move)
+				pgn: updatePGN(state[action.id].pgn, action.move)
 			}
 		};
 	}
