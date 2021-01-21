a6c4dd69a8e3a1ccf0fc7a73240ddf9838723c4e
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-20 21:57:21 -0500

    change port to 3001

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import WebSocket from 'ws';
 
 import { gameReducer } from './reducers/gameReducer.js';
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 3001 });
 let store = {};
 
 wss.on('connection', ws => {
     ws.on('message', message => {
         const action = JSON.parse(message);
+        console.log('message recieved: ', action);
         store.games = gameReducer(store.games, action, ws);
     });
 });
@@ -16,4 +17,4 @@ wss.on('close', ws => {
     // clean up 
 });
 
-console.log('WebSocket Server listening on port 8080...');
+console.log('WebSocket Server listening on port 3001...');
