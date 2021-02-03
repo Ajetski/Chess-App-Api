c8b7703eb1d6d8eed2fdd09806aaee0ecccf857d
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-02-02 23:45:10 -0500

    run linter (use tabs)

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,31 @@
 import * as Chess from 'chess.js';
 
 export function copyChess(chess: Chess.ChessInstance) {
-    const copy = new Chess.Chess();
-    copy.load_pgn(chess.pgn());
-    return copy;
+	const copy = new Chess.Chess();
+	copy.load_pgn(chess.pgn());
+	return copy;
 }
 
 export function updatePGN(pgn: string, move: string) {
-    const chess = new Chess.Chess();
-    chess.load_pgn(pgn);
-    chess.move(move);
-    return chess.pgn();
+	const chess = new Chess.Chess();
+	chess.load_pgn(pgn);
+	chess.move(move);
+	return chess.pgn();
 }
 
 export function pgnToColor(pgn: string) {
-    const chess = new Chess.Chess();
-    chess.load_pgn(pgn);
-    return chess.turn() === 'w' ? 'white' : 'black';
+	const chess = new Chess.Chess();
+	chess.load_pgn(pgn);
+	return chess.turn() === 'w' ? 'white' : 'black';
 }
 
 export function pgnToGameLength(pgn: string) {
-    const chess = new Chess.Chess();
-    chess.load_pgn(pgn);
-    return chess.history().length;
+	const chess = new Chess.Chess();
+	chess.load_pgn(pgn);
+	return chess.history().length;
+}
+
+export function getUptime(startTime: Date) {
+	const currTime = new Date();
+	return Math.round((currTime.getTime() - startTime.getTime()) / 1000);
 }
