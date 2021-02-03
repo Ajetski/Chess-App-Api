9d54422236b43db99f7ec5a6cc0c55362d3686ce
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-02-02 23:44:59 -0500

    run linter (use tabs)

--- src/actions/gameActions.ts
+++ src/actions/gameActions.ts
@@ -1,31 +1,31 @@
 export function makeMove({ pgn }: { pgn: string }) {
-    return JSON.stringify({
-        type: 'game/move',
-        pgn
-    });
+	return JSON.stringify({
+		type: 'game/move',
+		pgn
+	});
 }
 
 export function connectToGame({ id, orientation, isPlayer, pgn }: {
-    id: string, orientation: string, isPlayer: boolean, pgn: string
+	id: string, orientation: string, isPlayer: boolean, pgn: string
 }) {
-    return JSON.stringify({
-        type: 'game/connect',
-        id,
-        orientation,
-        isPlayer,
-        pgn
-    });
+	return JSON.stringify({
+		type: 'game/connect',
+		id,
+		orientation,
+		isPlayer,
+		pgn
+	});
 }
 
 export function stayConnected() {
-    return JSON.stringify({
-        type: 'game/pong'
-    });
+	return JSON.stringify({
+		type: 'game/pong'
+	});
 }
 
 export function error({ error }: { error: string }) {
-    return JSON.stringify({
-        type: 'game/error',
-        error
-    });
+	return JSON.stringify({
+		type: 'game/error',
+		error
+	});
 }
