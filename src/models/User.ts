c6c64bf082faba96277d3a151bfadb4d87734614
Caleb Garrick <caleb.garrick@gmail.com>
2021-02-09 22:55:53 -0500

    #18 init User schema

---
+++ src/models/User.ts
@@ -0,0 +1,47 @@
+import { Schema, Types } from 'mongoose';
+import validator from 'validator';
+
+export const User = new Schema({
+    uuid: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isUUID(value, 4)) {
+                throw new Error('Value must be a valid UUIDv4');
+            }
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isAlphanumeric(value)) {
+                throw new Error('Invalid Username');
+            }
+        }
+
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email');
+            }
+        }
+    },
+    elo: {
+        ranking: {
+            type: Types.Decimal128,
+            default: 1000.00,
+            required: true
+        },
+        provisional: {
+            type: Boolean,
+            default: true,
+            required: true
+        },
+        required: true
+    },
+});
+
