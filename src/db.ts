71445dcf5015ef12a1b4f8abaadae715795db348
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-02-10 09:59:24 -0500

    #13 fix db.ts connection

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,18 @@
 import { connect } from 'mongoose';
 
-console.log(`Connecting to ${process.env.DB_CONNECTION_URL}`);
-//ping
 connect(`${process.env.DB_CONNECTION_URL}`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: true
+        useFindAndModify: true,
+        authMechanism: 'SCRAM-SHA-1',
+        w: 'majority',
+        tls: true,
+        dbName: 'ajet-chess-app',
     })
     .then(() => {
         console.log('Connected to DB');
     })
-    .catch((err) => {
+    .catch((err: any) => {
         console.error(`Error connecting to DB: ${err}`);
     });
