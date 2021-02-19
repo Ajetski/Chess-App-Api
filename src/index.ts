4e537c454b096ffb12d5593fbedd1221d15a8d62
Caleb Garrick <caleb.garrick@gmail.com>
2021-02-19 16:10:21 -0500

    #14 remove game and user routes, import and use routers

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,25 @@
-import * as express from 'express';
-import * as ExpressWs from 'express-ws';
-import * as cors from 'cors';
-import * as bodyParser from 'body-parser';
+import express from 'express';
+import { Server } from 'ws';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 require('dotenv').config();
 import './db';
-import { gameReducer } from './reducers/gameReducer';
-import { pgnToGameLength, getUptime } from './utils';
-import { Action, Store } from './types/storeTypes';
-import { User } from './types/userTypes';
-import { User as UserModel } from './models/User';
+import { getUptime } from './utils';
+import { router as userRouter } from './routers/userRoutes';
+import { router as gameRouter, handleAction as handleGameAction, handleAction } from './routers/gameRoutes';
+import { Action } from './types/storeTypes';
 
-const app: any = express();
+const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
-const expressWs = ExpressWs(app);
+app.use(userRouter);
+app.use(gameRouter);
 
 const HTTP_PORT = process.env.PORT || 3001;
-
-let store: Store = { games: {} };
-let gameCounter = 1;
-
+const wss = new Server({ noServer: true });
 const startUpTime = new Date();
-
-app.ws('/', (ws: any, req: express.Request) => {
-	ws.on('message', (message: string) => {
-		const action: Action = JSON.parse(message);
-		console.log('message recieved: ', action);
-		store.games = gameReducer(store.games, action, ws);
-	});
-});
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	return res.send({
@@ -37,53 +27,20 @@ app.get('/', (req: express.Request, res: express.Response) => {
 	});
 });
 
-app.get('/game', (req: express.Request, res: express.Response) => {
-	return res.send(Object.keys(store.games).map((id: string) => ({
-		id,
-		numMoves: pgnToGameLength(store.games[parseInt(id)].pgn)
-	})));
+wss.on('connection', (ws: any) => {
+	ws.on('message', (message: string) => {
+		const action: Action = JSON.parse(message);
+		console.log('message recieved: ', action);
+		handleAction(action, ws);
+	});
 });
 
-app.post('/game', (req: express.Request<{}, {}, {
-	color?: 'white' | 'black' | 'random',
-	userId?: string
-}>, res: express.Response) => {
-	const id = gameCounter++;
-	store.games[id] = {
-		pgn: '',
-		white: {
-			userId: null,
-			connections: []
-		},
-		black: {
-			userId: null,
-			connections: []
-		},
-		spectators: []
-	};
-	if (req.body.userId) {
-		if (req.body.color === 'random') {
-			if (Math.floor(Math.random() * 10000) % 2 === 1) {
-				store.games[id].white.userId = req.body.userId;
-			} else {
-				store.games[id].black.userId = req.body.userId;
-			}
-		} else if (req.body.color === 'white' || req.body.color === 'black') {
-			store.games[id][req.body.color].userId = req.body.userId;
-		}
-	}
-	res.send({ id });
-});
 
-app.post('/user', async (req: express.Request<{}, {}, User>, res: express.Response) => {
-	try {
-		const user = new UserModel(req.body);
-		await user.save();
-		res.send(user);
-	} catch (err) {
-		// maybe log error to discord?
-		res.status(500).send(err);
-	}
-});
 
-app.listen(HTTP_PORT, () => console.log(`Http Server is listening on port ${HTTP_PORT}...`));
+const server = app.listen(HTTP_PORT, () => console.log(`Http Server is listening on port ${HTTP_PORT}...`));
+
+server.on('upgrade', (request, socket, head) => {
+	wss.handleUpgrade(request, socket, head, socket => {
+		wss.emit('connection', socket, request);
+	});
+});
